44f5fe3a524f5fe537d2d2f28a67ff8529cd8e1c
Yashwardhan-ed <180419645+Yashwardhan-ed@users.noreply.github.com>
2025-07-27 16:33:55 +0530

    Delete src/main/java/com/crm/config directory

--- src/main/java/com/crm/config/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package com.crm.config;
-
-public class DatabaseConfig {
-    public static final String URL = "jdbc:mysql://localhost:3306/crm_db?serverTimezone=UTC";
-    public static final String USER = "root";
-    public static final String PASSWORD = "Y@$HW@RDH@N$1ngh";
-}
